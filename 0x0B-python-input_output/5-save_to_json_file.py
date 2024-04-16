1804a3fe1c1fc18d9d3366c2fb6aa5144bc949dd
Emmanuel Odenyire Anyira <odenyirechristopher@gmail.com>
2024-04-16 08:27:15 -0700

    feat: Add functionality to save data to JSON file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,6 +4,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Function that writes an Object to a text file, using a json representation"""
+    """Function that writes an Object to a
+    text file, using a json representation"""
     with open(filename, mode="w") as f:
         json.dump(my_obj, f)
