640981a4ddb9b8543667bee18e3d273e5830895f
Emmanuel Odenyire Anyira <odenyirechristopher@gmail.com>
2024-04-16 11:04:31 -0700

    Some instantiation needed

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""Contains the Student class
+"""
+Module contains the Student class
 """
 
 
 class Student:
-    """Defines a student
+    """Defining a student
     """
     def __init__(self, first_name, last_name, age):
-        """Instanciates student attributes
+        """Instanciates and initializes 
+        the student attributes
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary
+        """Returns a dictionary
            representation of a Student
            instance
         """
