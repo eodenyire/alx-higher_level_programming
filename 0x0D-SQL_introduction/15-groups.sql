7657abca3400dcc92a173c86f9629d94f67369a9
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-05-21 22:20:50 +0300

    Pushing the file for task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,2 @@
--- lists number of records with the same score in the table second_table
--- list sorted by number of records descending
+-- List the number of records with the same score in second_table
 SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
