3403a151cf40a0f83049f8da91db7e74eb5323d1
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-05-21 21:22:52 +0300

    Little fixes on the script

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,3 @@
--- lists all the tables of a database in mysql server
--- database name will be passed as argument of mysql command
-SHOW tables;
+-- List all tables in the specified database
+USE $1;
+SHOW TABLES;
