851fa2ae46fa0eb40fceb8e6d7848268d78e23a1
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-05-21 21:31:34 +0300

    Update 7-insert_value.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
--- inserts a new row in the table first_table
--- database name is passes as argument of the mysql command
-INSERT INTO first_table (`id`, `name`) VALUES (89, "Holberton School");
+-- Insert a new row into first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+--INSERT INTO first_table (`id`, `name`) VALUES (89, "Best School");
