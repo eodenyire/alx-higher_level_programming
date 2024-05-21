1f78c8845ccf470bcdc5a062d28a1d3b9526abf7
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-05-21 21:43:59 +0300

    Different formatting options for the file 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,8 @@
--- lists all records of the table second_table
--- display both score and name, and database is passed as an argument of the mysql command
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- List all records in second_table ordered by score descending
+SELECT 
+  score, 
+  name 
+FROM second_table 
+ORDER BY score DESC;
+
+--SELECT score, name FROM second_table ORDER BY score DESC;
