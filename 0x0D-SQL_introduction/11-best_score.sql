05dab937d01adee3c0d9dbc99d847cf3cfa5a674
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-05-21 22:11:24 +0300

    Updating 11-best_score.sql file

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
--- lists all records with a score >= 10 in table second_table
--- display both the score and the name, and database is passed as an argument of the mysql command
+-- List records with score >= 10 in second_table ordered by score descending
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
