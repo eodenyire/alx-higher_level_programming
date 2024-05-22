4541cfdf640ca38f698bfcf6800347a8646f5329
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-05-23 00:08:42 +0300

    Updating 8-cities_of_california_subquery.sql for styling

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
---Select cities of California using subquery
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
 SELECT `id`, `name`
   FROM `cities`
  WHERE `state_id` IN
