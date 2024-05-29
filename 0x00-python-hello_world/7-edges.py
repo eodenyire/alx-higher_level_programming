d30865a28ca5b580906852c0aab98e50788b47f6
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-05-29 14:33:48 -0700

    Reformatting the file, Task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,8 +2,7 @@
 word = "Holberton"
 word_first_3 = word[:3]      # First 3 letters: "Hol"
 word_last_2 = word[-2:]      # Last 2 letters: "on"
-# Middle word without the first and last letters: "olberto"
-middle_word = word[1:-1]
+middle_word = word[1:-1]     # Middle word
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
