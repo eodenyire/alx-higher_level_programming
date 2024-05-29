293c4c09b34894b9fc70868030f58cc0c10cbfb5
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-05-29 14:30:06 -0700

    Pycodestyle on task 7

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,8 @@
 word = "Holberton"
 word_first_3 = word[:3]      # First 3 letters: "Hol"
 word_last_2 = word[-2:]      # Last 2 letters: "on"
-middle_word = word[1:-1]     # Middle word without the first and last letters: "olberto"
+# Middle word without the first and last letters: "olberto"
+middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
