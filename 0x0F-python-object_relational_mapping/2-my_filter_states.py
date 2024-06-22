9f392e5441e672d90ef3e138c9c501eab8a9642c
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-06-22 18:19:57 +0300

    Spacing

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,7 +3,6 @@
 Script that takes in an argument and displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
 """
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
