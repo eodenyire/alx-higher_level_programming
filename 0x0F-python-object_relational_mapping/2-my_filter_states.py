e6d2a2bfd0992f6798847f57ad4fb270524b0930
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-06-22 08:06:52 -0700

    Trying task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8")
+        db=sys.argv[3]
+        )
     cur = conn.cursor()
     cur.execute(
         "SELECT * FROM states WHERE name LIKE '%%{userinput}%%' \
