4889d14d64bafedf9d241e9cb124890d640ed537
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-04 23:13:29 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+"""
+Finds a peak in a list of unsorted integers
+"""
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
-
     if not list_of_integers:  # Handle empty list or None
         return None
 
@@ -15,3 +18,4 @@ def find_peak(list_of_integers):
             lo = mid + 1
 
     return list_of_integers[lo]
+
