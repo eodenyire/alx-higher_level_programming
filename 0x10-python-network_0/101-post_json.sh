34324ff0da12ff50bf6c40c72e30c67f0f83f14e
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-04 23:56:36 +0300

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # Sends a JSON POST request to a given URL with a given JSON file.
-curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
+
+# Validate if the JSON file is valid
+if ! jq . "$2" >/dev/null 2>&1; then
+    echo "Not a valid JSON"
+    exit 1
+fi
+
+# Send POST request with JSON file contents
+curl -s -H "Content-Type: application/json" -d "@$2" "$1"
