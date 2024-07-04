8e61299136eff9bdb364734d8a8e1d50c5071ba9
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-04 23:17:24 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""
-Finds a peak in a list of unsorted integers
+""" Finds a peak in a list of unsorted integers
 """
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-    if not list_of_integers:  # Handle empty list or None
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
 
-    lo, hi = 0, len(list_of_integers) - 1
-
-    while lo < hi:
-        mid = (lo + hi) // 2
-
-        if list_of_integers[mid] >= list_of_integers[mid + 1]:
-            hi = mid
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
         else:
-            lo = mid + 1
-
-    return list_of_integers[lo]
+            return list_of_integers[mid]
