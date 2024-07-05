245852668a621394c884e81dfbf6c329846d21d9
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-07-05 19:15:08 +0000

    Trying out task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,19 @@ Displays the type, content, and UTF-8 encoded content of the response.
 import urllib.request
 import urllib.error
 
-if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
-    
+
+def fetch_status(url):
+    """
+    Fetches data from a given URL and prints information about the response.
+
+    Args:
+    - url (str): The URL to fetch data from.
+
+    Prints:
+    - type of the content fetched.
+    - raw content fetched.
+    - UTF-8 decoded content fetched.
+    """
     try:
         with urllib.request.urlopen(url) as response:
             content = response.read()
@@ -16,6 +26,11 @@ if __name__ == "__main__":
             print(f"\t- type: {type(content)}")
             print(f"\t- content: {content}")
             print(f"\t- utf8 content: {content.decode('utf-8')}")
-    
+
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    fetch_status(url)
