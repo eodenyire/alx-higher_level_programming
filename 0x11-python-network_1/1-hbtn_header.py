ee518173cc6266e6b99acb31175ac643af73bd8c
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-05 23:10:29 +0300

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays
-the value of X-Request-Id varialble found in the header
-of the response
+"""
+This script takes in a URL, sends a request to the URL,
+and displays the value of the 'X-Request-Id' variable found
+in the header of the response.
+
+Usage:
+    ./script_name.py <URL>
+
+Example:
+    ./script_name.py https://alx-intranet.hbtn.io
 """
 if __name__ == "__main__":
     import sys
