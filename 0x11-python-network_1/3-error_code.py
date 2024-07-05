eccf19cb9565bf28695552782981663ea92abd94
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-05 23:44:55 +0300

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-"""
-Takes in a URL, sends a request to the URL, and displays the body of the response (decoded in utf-8).
-If an HTTP error occurs, it prints the error code.
-
-Usage:
-    ./script_name.py <URL>
-
-Example:
-    ./script_name.py http://example.com
-
-This script sends a GET request to the specified URL and prints the response body.
-If the request results in an HTTP error, it prints the error code instead.
+"""Takes in a URL, sends a request to the URL
+and displays the body of the response
 """
 if __name__ == "__main__":
     import sys
