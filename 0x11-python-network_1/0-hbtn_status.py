49c0ec4211336badc8a03650f74dca113613b420
Emmanuel Odenyire Anyira <61192633+eodenyire@users.noreply.github.com>
2024-07-05 21:31:04 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using the urllib package
+"""
+Fetches data from https://alx-intranet.hbtn.io/status
+Displays the type, content, and UTF-8 encoded content of the response.
 """
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    data = response.read()
-    print("Body response:")
-    print("    - type:", type(data))
-    print("    - content:", data)
-    print("    - utf8 content:", data.decode('utf-8'))
+import urllib.error
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read()
+            print("Body response:")
+            print(f"\t- type: {type(content)}")
+            print(f"\t- content: {content}")
+            print(f"\t- utf8 content: {content.decode('utf-8')}")
+    
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
