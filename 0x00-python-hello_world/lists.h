3f5f76eb8fb4a1c0b281b8c74d3e4611aa80a86a
Emmanuel Odenyire <61192633+eodenyire@users.noreply.github.com>
2025-01-26 01:21:47 +0300

    Update lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,20 +4,16 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
+ *struct listint_s - a singly linked list
+ * @n: number integer
+ * @next: pointer pointing to the next node
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+	    int n;
+	    struct listint_s *next;
 } listint_t;
 
-int check_cycle(listint_t *list);
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
